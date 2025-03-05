e4dbad9e15dc67c710dc152c597c3a0c45f53fdf
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-05 14:53:16 +0530

    [Dhani] Add. random function to get two dice numbers and add them

---
+++ JavaScript_Constructs/Sequence_Problems/AddTwoDiceNumbers.js
@@ -0,0 +1,8 @@
+//dice number 1
+let diceNumber1= Math.floor(Math.random()*6)+1;
+//dice number 2
+let diceNumber2= Math.floor(Math.random()*6)+1;
+//sum of two dice numbers
+let sum=diceNumber1+diceNumber2;
+//print sum of two dice numbers
+console.log("Sum of two dice numbers is: "+sum);
