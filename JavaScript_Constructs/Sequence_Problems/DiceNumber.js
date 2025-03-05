95545a9ce54d7e4902262a5c64e685e411c9f89b
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-05 14:52:39 +0530

    [Dhani] Add. random function to get dice number

---
+++ JavaScript_Constructs/Sequence_Problems/DiceNumber.js
@@ -0,0 +1,5 @@
+console.log("Starting the program");
+//random function to get dice number between 1 to 6
+let diceNumber= Math.floor(Math.random()*6)+1;
+//print dice number
+console.log("Dice Number:"+diceNumber);
