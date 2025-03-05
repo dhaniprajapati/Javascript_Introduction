776519077f4314cd49b8d55bb3bb2068b00e9d7b
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-05 16:31:21 +0530

    [Dhani] Add. generate 10 random no. and store them in array. Find 2nd largest and smallest too.

---
+++ JavaScript_DS/Arrays/RandomNumbers.js
@@ -0,0 +1,31 @@
+//generate 10 random numbers
+for(let i=0;i<10;i++){
+    let randomNumbers= Math.floor(Math.random()*900)+100;
+    console.log("10 random numbers: "+randomNumbers);
+}
+//store the generated rabndomNumbers in an array
+let randomNumbersArray=new Array();
+for(let i=0;i<10;i++){
+    let randomNumbers= Math.floor(Math.random()*900)+100;
+    randomNumbersArray.push(randomNumbers);
+}
+//find the second largest and second smallest number
+let largest=randomNumbersArray[0];
+let smallest=randomNumbersArray[0];
+let secondLargest=randomNumbersArray[0];
+let secondSmallest=randomNumbersArray[0];
+for(let i=0;i<randomNumbersArray.length;i++){
+    if(randomNumbersArray[i]>largest){
+        secondLargest=largest;
+        largest=randomNumbersArray[i];
+    }else if(randomNumbersArray[i]>secondLargest && randomNumbersArray[i]!=largest){
+        secondLargest=randomNumbersArray[i];
+    }
+    if(randomNumbersArray[i]<smallest){
+        secondSmallest=smallest;
+        smallest=randomNumbersArray[i];
+    }else if(randomNumbersArray[i]<secondSmallest && randomNumbersArray[i]!=smallest){
+        secondSmallest=randomNumbersArray[i];
+    }
+}
+
